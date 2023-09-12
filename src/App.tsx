59d1ae9bfe0d03e69f744e5a3d6ba8ee369d8e40
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-09-12 11:24:49 -0300

    Add prompt & AI generated result text areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { Header } from './components/header'
+import { Textarea } from './components/ui/textarea'
 
 export function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
 
-      {/* <div className="flex-1">oi</div> */}
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Add AI prompt"
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="AI generated result"
+              readOnly
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Tip: you can use the variable{' '}
+            <code className="font-violet-400">{'{transcription}'}</code> within
+            your prompt to add content based on the selected video transcription
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </div>
   )
 }
